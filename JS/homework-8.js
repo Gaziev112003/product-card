f7c6155c86419359575280268208c3392379e962
Gaziev112003 <gaziev736@gmail.com>
2026-08-24 16:46:50 +0300

    add fils homework-8.js, add objects and array

---
+++ JS/homework-8.js
@@ -0,0 +1,147 @@
+// 3. Создайте объект на основе ваших данных.
+
+const userProfile = {
+  firstName: "Amir",
+  lastName: "Gaziev",
+  age: 22,
+  email: "gaziev736@gmail.com",
+  job: "Frontend Developer",
+  country: "Russia",
+  city: "Kizlyar",
+  hobbies: "football",
+};
+
+// 4. Создайте объект, который будет хранить данные об автомобиле.
+
+const carInfo = {
+  brand: "BMW",
+  model: "m5 F90",
+  year: 2023,
+  color: "grey",
+  transmission: "automatic",
+};
+
+// Добавляем объект userProfile в объект carInfo
+
+const updatedCarInfo = { ...carInfo, userCar: userProfile };
+
+// 5. Создайте функцию, которая будет проверять наличие свойства "maxSpeed".
+
+function checkMaxSpeed(carInfo) {
+
+  if ("maxSpeed" in carInfo) {
+    return;
+  }
+
+  carInfo.maxSpeed = 360;
+}
+
+// Вызов функции checkMaxSpeed.
+
+checkMaxSpeed(carInfo);
+
+// 6. Написать функцию где ты пишешь наз.объекта и наз.ключа, а она тебе дает свойство.
+
+function printValue(object, key) {
+  console.log(object[key]);
+}
+
+printValue(carInfo, "brand");
+
+// 7. Создать массив, который содержит названия продуктов.
+
+const products = ["Мука", "Сахар", "Соль", "Рис", "Молоко"];
+
+// 8. Создать массив, состоящий из объектов
+
+const carList = [
+{
+  brand: "BMW",
+  model: "m5 F90",
+  year: 2023,
+  color: "grey",
+  transmission: "automatic",
+  made: "Germany",
+  circulation: "unlimited",
+},
+{
+  brand: "Mercedes-Benz",
+  model: "CLS AMG",
+  year: 2021,
+  color: "black",
+  transmission: "automatic",
+  made: "Germany",
+  circulation: "unlimited",
+},
+{
+  brand: "Porsche",
+  model: "911 GT3-RS",
+  year: 2024,
+  color: "silver",
+  transmission: "automatic",
+  made: "Germany",
+  circulation: "unlimited",
+},
+]
+
+// добавить еще один список объекта в в конце масива
+
+carList.push({
+  brand: "Audi",
+  model: "RS-7",
+  year: 2022,
+  color: "black-grin",
+  transmission: "automatic",
+  made: "Germany",
+  circulation: "unlimited",
+})
+
+// 9. Создать еще один массив, ...
+
+const superCarList = [
+{
+  brand: "Bugatti",
+  model: "Tourbillon",
+  year: 2026,
+  color: "blue",
+  transmission: "DCT",
+  made: "France",
+  circulation: "limited",
+},
+{
+  brand: "Koenigsegg",
+  model: "Jesko Absolut",
+  year: 2024,
+  color: "white",
+  transmission: "LST",
+  made: "Sweden",
+  circulation: "limited",
+},
+]
+
+// объединить эти два массива в один
+
+const allCars = [...carList, ...superCarList];
+
+// 10. Почитать про метод массива — map. Написать функцию.
+
+function rareCars(objCars) {
+  return objCars.map(function(car) {
+    const updatedAllCar = {...car};
+
+    if (updatedAllCar.circulation === 'limited') {
+      updatedAllCar.isRare = true;
+    }
+    else {
+      updatedAllCar.isRare = false;
+    }
+    return updatedAllCar;
+  });
+}
+
+// Вызов функции
+
+const updatedAllCar = rareCars(allCars);
+
+
+
